bc1f734aa129c62dc07e2a38ebf32f21f3b6a35c
Lauri Törmä <bgu171@myy.haaga-helia.fi>
2022-10-31 08:28:21 +0200

    First commit

---
+++ src/main/java/com/example/HauvatarStore/HauvatarStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.HauvatarStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HauvatarStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauvatarStoreApplication.class, args);
+	}
+
+}
