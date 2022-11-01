bdd9bd53fe451f9b715b5b37518c57bb9703d9b6
LassiMik <112076377+LassiMik@users.noreply.github.com>
2022-11-01 17:13:52 +0200

    Controller

---
+++ HauvatarStore-master/src/main/java/com/example/HauvatarStore/HauvatarStoreApplication.java
@@ -0,0 +1,47 @@
+package com.example.HauvatarStore;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.HauvatarStore.domain.Garmet;
+import com.example.HauvatarStore.domain.GarmetRepository;
+
+// import com.example.HauvatarStore.domain.Tuote;
+// import com.example.HauvatarStore.domain.TuoteRepository;
+// import com.example.HauvatarStore.domain.Category;
+// import com.example.HauvatarStore.domain.CategoryRepository;
+// import com.example.HauvatarStore.domain.Useri;
+// import com.example.HauvatarStore.domain.UserRepository;
+
+@SpringBootApplication
+public class HauvatarStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HauvatarStoreApplication.class, args);
+	}
+	@Bean
+	public CommandLineRunner vaateDemo(GarmetRepository gRepository) {
+		//Lassin testisäätö
+		return (args) -> {
+			gRepository.save(new Garmet("asd", "ASd", 123, "asd"));
+		};
+	}
+	//@Bean
+	//public CommandLineRunner TuoteDemo(TuoteRepository tRepository, CategoryRepository categoryRepository, UserRepository urepository) {
+		//return (args) -> {
+			// categoryRepository.save(new Category("Koiran Vaatteet"));
+			// categoryRepository.save(new Category("Kissan Vaatteet"));
+			
+			//tRepository.save(new Tuote("Koiran Villapaita", categoryRepository.findByName("Koiran Vaatteet").get(0), "15.99", "HauHau"));
+			//tRepository.save(new Tuote("Koiran Sadetakki", categoryRepository.findByName("Koiran Vaatteet").get(0), "29.99", "HauHau"));
+		
+			//Useri user1 = new Useri("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			//Useri user2 = new Useri("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			//urepository.save(user1);
+			//urepository.save(user2);
+		//};
+	//}
+
+}
