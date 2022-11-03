97acd6ca013c00e72c74e78112b71d2405931b53
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-11-03 17:55:00 +0200

    added new ManufacturerController and mappings to view, add, edit and delete manufacturers

--- src/main/java/com/example/HauvatarStore/domain/ManufacturerRepository.java
+++ src/main/java/com/example/HauvatarStore/domain/ManufacturerRepository.java
@@ -5,5 +5,5 @@ import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ManufacturerRepository extends CrudRepository <Manufacturer, Long> {
-	List<Manufacturer> findByName(String manufacturerName);
+	List<Manufacturer> findByManufacturerName(String manufacturerName);
 }
---
+++ src/main/java/com/example/HauvatarStore/web/ManufacturerController.java
@@ -0,0 +1,69 @@
+package com.example.HauvatarStore.web;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.HauvatarStore.domain.Manufacturer;
+import com.example.HauvatarStore.domain.ManufacturerRepository;
+
+@Controller
+public class ManufacturerController {
+
+	
+	@Autowired
+	private ManufacturerRepository mrepository;
+	
+	
+	//View all manufacturers in list
+	
+	 @RequestMapping(value= {"/manufacturers","/manufacturerList"})
+	    public String manufacturerList(Model model) {	
+	        model.addAttribute("manufacturers", mrepository.findAll());
+	        return "/manufacturerList";
+	    }
+	 
+	 //Add new manufacturer
+	 
+	 @RequestMapping(value = "/addManufacturer")
+	    public String addManufacturer(Model model){
+	    	model.addAttribute("manufacturer", new Manufacturer());
+	        return "/addManufacturer";  
+	    }
+	 
+	 //Save new manufacturer
+	 
+	 @RequestMapping(value = "/saveManufacturer", method = RequestMethod.POST)
+	    public String save(@ModelAttribute ("manufacturer") @Valid Manufacturer manufacturer, BindingResult bindingResult){
+	    	if (bindingResult.hasErrors()) {
+	    		return "/addManufacturer";
+	    	}
+	    	mrepository.save(manufacturer);
+	        return "redirect:/manufacturerList";
+	    }
+	 
+	 //Edit manufacturer
+	 
+	 @RequestMapping(value = "/edit/{manufacturerId}", method = RequestMethod.GET)
+	    public String editManufacturer(@PathVariable("manufacturerId") Long manufacturerId, Model model) { 
+	    	model.addAttribute("manufacturer", mrepository.findById(manufacturerId));
+	        return "/editManufacturer";
+	    }
+	 
+	 //Delete manufacturer
+	 
+	 @RequestMapping(value = "/delete/{manufacturerId}", method = RequestMethod.GET)
+	    public String deleteManufacturer(@PathVariable("manufacturerId") Long manufacturerId, Model model) {
+	    	mrepository.deleteById(manufacturerId);
+	        return "redirect:/clothes";
+	    }
+	 
+	 
+}
