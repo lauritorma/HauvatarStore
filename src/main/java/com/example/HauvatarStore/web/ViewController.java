eae33fa2b233892fb041ce281e327c3706832372
LassiMik <lassi@vartiaiset.com>
2022-11-17 21:05:07 +0200

    added mobile views

---
+++ src/main/java/com/example/HauvatarStore/web/ViewController.java
@@ -0,0 +1,31 @@
+package com.example.HauvatarStore.web;
+
+import org.springframework.mobile.device.Device;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String greeting(Device device) {
+		
+        String deviceType = "browser";
+        String platform = "browser";
+        String viewName = "homepage";
+		
+        if (device.isNormal()) {
+            deviceType = "browser";
+        } else if (device.isMobile()) {
+            deviceType = "mobile";
+            viewName = "mobile/homepage";
+        } 
+        platform = device.getDevicePlatform().name();
+        
+        if (platform.equalsIgnoreCase("UNKNOWN")) {
+            platform = "browser";
+        }
+     	
+        return viewName;
+    }
+}
