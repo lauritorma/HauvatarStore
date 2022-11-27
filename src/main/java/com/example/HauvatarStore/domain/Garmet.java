741dbf41d1ae17e89d50072df7e542aead46f4cf
Lauri Törmä <90974678+lauritorma@users.noreply.github.com>
2022-11-27 20:32:40 +0200

    adding table annotations

--- src/main/java/com/example/HauvatarStore/domain/Garmet.java
+++ src/main/java/com/example/HauvatarStore/domain/Garmet.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -11,6 +12,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
 @Entity
+@Table(name="Garmet")
 public class Garmet {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- src/main/java/com/example/HauvatarStore/domain/Manufacturer.java
+++ src/main/java/com/example/HauvatarStore/domain/Manufacturer.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import org.springframework.lang.NonNull;
@@ -15,6 +16,7 @@ import org.springframework.lang.NonNull;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name="Manufacturer")
 public class Manufacturer {
 
 	@Id
