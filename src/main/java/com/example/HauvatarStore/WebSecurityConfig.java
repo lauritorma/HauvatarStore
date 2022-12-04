55709059301433ad9c4c277099cbee2401b2b0fc
SeikkuS <bgu221@myy.haaga-helia.fi>
2022-12-04 10:47:09 +0200

    Spring Security + WebConfig

---
+++ src/main/java/com/example/HauvatarStore/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.example.HauvatarStore;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.HauvatarStore.web.UserDetailServiceImpl;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SuppressWarnings("deprecation")
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserDetailServiceImpl userDetailsService;	
+	
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+        .authorizeRequests().antMatchers("/css/**").permitAll() // Enable css when logged out
+        .and()
+        .authorizeRequests().antMatchers("/signup", "/saveuser").permitAll()
+        .and()
+        .authorizeRequests().anyRequest().authenticated()
+        .and()
+      .formLogin()
+          .loginPage("/login")
+          .defaultSuccessUrl("/gamelist", true)
+          .permitAll()
+          .and()
+      .logout()
+          .permitAll();
+    }
+    
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
--- src/main/java/com/example/HauvatarStore/web/UserDetailServiceImpl.java
+++ src/main/java/com/example/HauvatarStore/web/UserDetailServiceImpl.java
@@ -1,1 +1,32 @@
-//todo
+package com.example.HauvatarStore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.HauvatarStore.domain.UserClass;
+import com.example.HauvatarStore.domain.UserRepository;
+
+/**
+ * This class is used by spring security to authenticate and authorize user
+ **/
+@Service
+public class UserDetailServiceImpl implements UserDetailsService  {
+	private final UserRepository repository;
+
+	@Autowired
+	public UserDetailServiceImpl(UserRepository userRepository) {
+		this.repository = userRepository;
+	}
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {   
+    	UserClass curruser = repository.findByUsername(username);
+        UserDetails user = new org.springframework.security.core.userdetails.User(username, curruser.getPasswordHash(), 
+        		AuthorityUtils.createAuthorityList(curruser.getRole()));
+        return user;
+    }   
+} 
