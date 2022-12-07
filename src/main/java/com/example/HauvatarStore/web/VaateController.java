15d91b6de6e0fc1820d5133488147dc67cb959d8
Jeremy <jerkku_astrom@hotmail.com>
2022-12-07 17:57:10 +0200

    lisätty login

--- src/main/java/com/example/HauvatarStore/web/VaateController.java
+++ src/main/java/com/example/HauvatarStore/web/VaateController.java
@@ -23,7 +23,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.HauvatarStore.domain.Garmet;
 import com.example.HauvatarStore.domain.GarmetRepository;
@@ -31,89 +30,95 @@ import com.example.HauvatarStore.domain.ManufacturerRepository;
 
 @Controller
 public class VaateController {
-    @Autowired
-    private GarmetRepository garmetRepository;
-    @Autowired
-    private ManufacturerRepository mrepository;
+	@Autowired
+	private GarmetRepository garmetRepository;
+	@Autowired
+	private ManufacturerRepository mrepository;
 
-    @RequestMapping(value = { "/","/favicon.ico","/index" })
-    public String homepage(Model model) {
-        return "/homepage";
-    }
+	@RequestMapping(value = { "/", "/favicon.ico", "/index" })
+	public String homepage(Model model) {
+		return "/homepage";
+	}
 
-    @RequestMapping(value = { "/clothes", "/clothelist" })
-    public String garmetList(Model model) {
-        model.addAttribute("clothes", garmetRepository.findAll());
-        return "/clothelist";
-    }
-    
-    @RequestMapping(value = { "/clothesByManufacturer/{manufacturer}" })
-    public String garmetListByManufacturer(@PathVariable("manufacturer") String manufacturer, Model model) {
-        model.addAttribute("clothes", garmetRepository.findByManufacturer(manufacturer));
-        return "/clothelist";
-    }
+	@RequestMapping(value = { "/clothes", "/clothelist" })
+	public String garmetList(Model model) {
+		model.addAttribute("clothes", garmetRepository.findAll());
+		return "/clothelist";
+	}
 
-    @RequestMapping(value = "/add")
-    public String addGarmet(Model model) {
-        model.addAttribute("garmet", new Garmet());
-        model.addAttribute("manufacturers", mrepository.findAll());
-        return "/addClothe";
-    }
+	@RequestMapping(value = { "/clothesByManufacturer/{manufacturer}" })
+	public String garmetListByManufacturer(@PathVariable("manufacturer") String manufacturer, Model model) {
+		model.addAttribute("clothes", garmetRepository.findByManufacturer(manufacturer));
+		return "/clothelist";
+	}
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public String save(Model model, @ModelAttribute("garmet") @Valid Garmet garmet, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            model.addAttribute("manufacturers", mrepository.findAll());
-            return "/addClothe";
-        }
-        garmetRepository.save(garmet);
-        return "redirect:/clothes";
-    }
+	@RequestMapping(value = "/add")
+	public String addGarmet(Model model) {
+		model.addAttribute("garmet", new Garmet());
+		model.addAttribute("manufacturers", mrepository.findAll());
+		return "/addClothe";
+	}
 
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public String deleteGarmet(@PathVariable("id") Long garmetId, Model model) {
-        garmetRepository.deleteById(garmetId);
-        return "redirect:/clothes";
-    }
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String save(Model model, @ModelAttribute("garmet") @Valid Garmet garmet, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("manufacturers", mrepository.findAll());
+			return "/addClothe";
+		}
+		garmetRepository.save(garmet);
+		return "redirect:/clothes";
+	}
 
-    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
-    public String editGarmet(@PathVariable("id") Long garmetId, Model model) {
-        model.addAttribute("garmet", garmetRepository.findById(garmetId));
-        model.addAttribute("manufacturers", mrepository.findAll());
-        return "/editClothe";
-    }
-    
-    //REST FUNCTIONALITY---------------------------------------------------------------------------------------------------------------------------------------------
-    
-    @GetMapping(value = { "rest/getAllClothes" })
-    public @ResponseBody List<Garmet> getAllClothes() {
-        return (List<Garmet>) garmetRepository.findAll();
-    }
-    
-    @GetMapping(value = { "rest/getClothe/{id}" })
-    public @ResponseBody Optional<Garmet> getClotheById(@PathVariable("id") Long garmetId) {
-    	return garmetRepository.findById(garmetId);
-    }
-    
-    @PostMapping(value = { "rest/postNewClothe" })
-    public ResponseEntity<Garmet> postNewClothe(@RequestBody Garmet newGarmet) {
-    	Garmet savedGarmet = garmetRepository.save(newGarmet);
-    	return ResponseEntity.created(URI.create(String.format("/garmet/%s", newGarmet.getId()))).body(savedGarmet);
-    }
-    
-    @DeleteMapping(value = { "rest/deleteClothe/{id}" })
-    public @ResponseBody void deleteClotheById(@PathVariable("id") Long garmetId) {
-        garmetRepository.deleteById(garmetId);
-    }
-    
-    @PutMapping(value = { "rest/putClothe/{id}" })
-    public ResponseEntity<?> editClotheById(@RequestBody Garmet newGarmet, @PathVariable Long id) {
-    	try {
-    		newGarmet.setId(id);            
-            garmetRepository.save(newGarmet);
-            return new ResponseEntity<>(HttpStatus.OK);
-        } catch (NoSuchElementException e) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteGarmet(@PathVariable("id") Long garmetId, Model model) {
+		garmetRepository.deleteById(garmetId);
+		return "redirect:/clothes";
+	}
+
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+	public String editGarmet(@PathVariable("id") Long garmetId, Model model) {
+		model.addAttribute("garmet", garmetRepository.findById(garmetId));
+		model.addAttribute("manufacturers", mrepository.findAll());
+		return "/editClothe";
+	}
+
+	@GetMapping(value = "/login")
+	public String login() {
+		return "login";
+	}
+
+	// REST
+	// FUNCTIONALITY---------------------------------------------------------------------------------------------------------------------------------------------
+
+	@GetMapping(value = { "rest/getAllClothes" })
+	public @ResponseBody List<Garmet> getAllClothes() {
+		return (List<Garmet>) garmetRepository.findAll();
+	}
+
+	@GetMapping(value = { "rest/getClothe/{id}" })
+	public @ResponseBody Optional<Garmet> getClotheById(@PathVariable("id") Long garmetId) {
+		return garmetRepository.findById(garmetId);
+	}
+
+	@PostMapping(value = { "rest/postNewClothe" })
+	public ResponseEntity<Garmet> postNewClothe(@RequestBody Garmet newGarmet) {
+		Garmet savedGarmet = garmetRepository.save(newGarmet);
+		return ResponseEntity.created(URI.create(String.format("/garmet/%s", newGarmet.getId()))).body(savedGarmet);
+	}
+
+	@DeleteMapping(value = { "rest/deleteClothe/{id}" })
+	public @ResponseBody void deleteClotheById(@PathVariable("id") Long garmetId) {
+		garmetRepository.deleteById(garmetId);
+	}
+
+	@PutMapping(value = { "rest/putClothe/{id}" })
+	public ResponseEntity<?> editClotheById(@RequestBody Garmet newGarmet, @PathVariable Long id) {
+		try {
+			newGarmet.setId(id);
+			garmetRepository.save(newGarmet);
+			return new ResponseEntity<>(HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
